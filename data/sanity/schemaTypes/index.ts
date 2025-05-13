b3188c1c3cf86376a73b25f28b5189b8c1e76a05
codinister <condinister@gmail.com>
2025-05-13 19:38:07 +0100

    Commit 5

---
+++ data/sanity/schemaTypes/index.ts
@@ -0,0 +1,10 @@
+import blockContent from './blockContent';
+import category from './category';
+import post from './post';
+import author from './author';
+import slider from './slider';
+import contact from './contact';
+import item from './item';
+import bestselling from './bestselling';
+
+export const schemaTypes = [post,item, author, category, blockContent,contact,slider,bestselling];
